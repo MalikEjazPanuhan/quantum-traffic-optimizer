600fef3476a7ea6b25b9d4928e3d160278317ea6
Muhammad Ejaz <malikejaz6858@gmail.com>
2026-08-09 02:45:18 -0700

    Import render_template in app.py

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, render_template_string
+from flask import Flask, jsonify, request, render_template_string, render_template
 from flask_cors import CORS
 import sys
 import os
